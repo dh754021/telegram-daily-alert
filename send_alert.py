ac0203ec1c3427a30d3af465ce99037a26383501
dh754021 <dh754021@gmail.com>
2026-08-28 02:01:27 +0900

    Update send_alert.py

--- send_alert.py
+++ send_alert.py
@@ -1,1 +1,8 @@
- import requests import os TOKEN = os.environ['BOT_TOKEN'] CHAT_ID = '-5137059226' url = f'https://api.telegram.org/bot{TOKEN}/sendMessage' requests.post(url, data={'chat_id': CHAT_ID, 'text': '콜 보내야함'})
+import requests
+import os
+
+TOKEN = os.environ['BOT_TOKEN']
+CHAT_ID = '-5137059226'
+
+url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+requests.post(url, data={'chat_id': CHAT_ID, 'text': '콜 보내야함'})
