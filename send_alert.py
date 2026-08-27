e225d337a60361a927168877c2d86b96af40ff2a
dh754021 <dh754021@gmail.com>
2026-08-28 02:08:57 +0900

    Update send_alert.py

--- send_alert.py
+++ send_alert.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 TOKEN = os.environ['BOT_TOKEN']
-CHAT_ID = '8356370345'
+CHAT_ID = '-100247365139'
 
 url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 requests.post(url, data={'chat_id': CHAT_ID, 'text': '콜 보내야함'})
