0cf3d98b97db6495bd01adcc2970548968b5e09b
dh754021 <dh754021@gmail.com>
2026-08-28 01:44:03 +0900

    Create  send_alert.py

---
+++ send_alert.py
@@ -0,0 +1,1 @@
+ import requests import os TOKEN = os.environ['BOT_TOKEN'] CHAT_ID = '-5137059226' url = f'https://api.telegram.org/bot{TOKEN}/sendMessage' requests.post(url, data={'chat_id': CHAT_ID, 'text': '콜 보내야함'})
